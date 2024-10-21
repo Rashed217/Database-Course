bd41551b1f6534d0afdc0136ddcd383e3befe60f
Rashed Al Ghailani <147449418+Rashed217@users.noreply.github.com>
2024-10-21 16:20:02 +0400

    Add files via upload
    Aggregate Function & Grouping Task

---
+++ Aggregate Function & Grouping Task.sql
@@ -0,0 +1,13 @@
+Use Company_SD
+
+
+--1 
+Select Sum(W.Hours) as [Sum of Hours], P.Pname as [Project Name]
+From Works_for W inner join Project P on W.Pno = P.Pnumber
+Group by P.Pname
+
+
+--2
+Select D.Dname as [Dept. Name], Max(E.Salary) as [Max Salary], Min(E.Salary) as [Min Salary], AVG(E.Salary) as [Avg Salary]
+From Departments D inner join Employee E on D.Dnum = E.Dno
+Group by D.Dname
